b44d5b8f0d4b5ea43407af9c220e4e02454f5557
morris <morris@94223b32-352b-4d6d-a509-7b239cf53eea>
2009-02-18 00:24:31 +0000

    Add accounts pane to administration, doesn't work yet
    git-svn-id: file:///sources/wired/zanka-full-svn/branches/P7/WiredClient@6912 94223b32-352b-4d6d-a509-7b239cf53eea

--- WCAccountsController.h
+++ WCAccountsController.h
@@ -29,6 +29,94 @@
 #import "WCAdministration.h"
 
 @interface WCAccountsController : WCAdministrationController {
+	IBOutlet NSButton					*_allFilterButton;
+	IBOutlet NSButton					*_usersFilterButton;
+	IBOutlet NSButton					*_groupsFilterButton;
+	IBOutlet NSPopUpButton				*_groupFilterPopUpButton;
+	IBOutlet NSMenuItem					*_anyGroupMenuItem;
+	IBOutlet NSMenuItem					*_noGroupMenuItem;
+	IBOutlet NSSearchField				*_filterSearchField;
+
+	IBOutlet WITableView				*_accountsTableView;
+	
+	IBOutlet NSTabView					*_accountsTabView;
+
+	IBOutlet NSProgressIndicator		*_progressIndicator;
+	IBOutlet NSButton					*_saveButton;
+
+	IBOutlet NSPopUpButton				*_typePopUpButton;
+	IBOutlet NSMenuItem					*_userMenuItem;
+	IBOutlet NSMenuItem					*_groupMenuItem;
+
+	IBOutlet NSTextField				*_nameTextField;
+	IBOutlet NSTextField				*_fullNameTextField;
+	IBOutlet NSTextField				*_creationTimeTextField;
+	IBOutlet NSTextField				*_modificationTimeTextField;
+	IBOutlet NSTextField				*_loginTimeTextField;
+	IBOutlet NSTextField				*_editedByTextField;
+	IBOutlet NSSecureTextField			*_passwordTextField;
+	IBOutlet NSPopUpButton				*_groupPopUpButton;
+	IBOutlet NSMenuItem					*_noneMenuItem;
+	IBOutlet NSTokenField				*_groupsTokenField;
+	
+	IBOutlet NSPopUpButton				*_showPopUpButton;
+	IBOutlet NSMenuItem					*_allSettingsMenuItem;
+	IBOutlet NSMenuItem					*_settingsDefinedAtThisLevelMenuItem;
+	
+	IBOutlet WIOutlineView				*_settingsOutlineView;
+	IBOutlet NSTableColumn				*_settingTableColumn;
+	IBOutlet NSTableColumn				*_valueTableColumn;
+	
+	NSArray								*_allSettings;
+	NSMutableArray						*_shownSettings;
+	
+	NSMutableArray						*_allAccounts;
+	NSMutableArray						*_shownAccounts;
+	NSImage								*_userImage;
+	NSImage								*_groupImage;
+	
+	WCAccount							*_account;
+	WCAccount							*_underlyingAccount;
+	
+	BOOL								_editingAccount;
+	BOOL								_creatingAccount;
+	BOOL								_accountTouched;
+
+	BOOL								_received;
+
+	NSString							*_accountFilter;
+	WIDateFormatter						*_dateFormatter;
 }
+
+- (NSArray *)accounts;
+- (NSArray *)users;
+- (NSArray *)userNames;
+- (NSArray *)groups;
+- (NSArray *)groupNames;
+- (WCAccount *)userWithName:(NSString *)name;
+- (WCAccount *)groupWithName:(NSString *)name;
+- (void)editUserAccountWithName:(NSString *)name;
+
+- (IBAction)touch:(id)sender;
+- (IBAction)add:(id)sender;
+- (IBAction)delete:(id)sender;
+- (IBAction)reload:(id)sender;
+- (IBAction)changePassword:(id)sender;
+- (IBAction)submitPasswordSheet:(id)sender;
+- (IBAction)all:(id)sender;
+- (IBAction)users:(id)sender;
+- (IBAction)groups:(id)sender;
+- (IBAction)groupFilter:(id)sender;
+- (IBAction)search:(id)sender;
+- (IBAction)type:(id)sender;
+- (IBAction)group:(id)sender;
+- (IBAction)show:(id)sender;
+- (IBAction)clearSetting:(id)sender;
+- (IBAction)save:(id)sender;
+
+@end
+
+
+@interface WCAccountsTableColumn : NSTableColumn
 
 @end
--- WCAccountsController.m
+++ WCAccountsController.m
@@ -44,4 +44,314 @@
 
 @implementation WCAccountsController
 
+- (NSArray *)accounts {
+	return NULL;
+}
+
+
+
+- (NSArray *)users {
+	return NULL;
+}
+
+
+
+- (NSArray *)userNames {
+	return NULL;
+}
+
+
+
+- (NSArray *)groups {
+	return NULL;
+}
+
+
+
+- (NSArray *)groupNames {
+	return NULL;
+}
+
+
+
+- (WCAccount *)userWithName:(NSString *)name {
+	return NULL;
+}
+
+
+
+- (WCAccount *)groupWithName:(NSString *)name {
+	return NULL;
+}
+
+
+
+- (void)editUserAccountWithName:(NSString *)name {
+}
+
+
+
+#pragma mark -
+
+- (IBAction)touch:(id)sender {
+}
+
+
+
+- (IBAction)add:(id)sender {
+}
+
+
+
+- (IBAction)delete:(id)sender {
+}
+
+
+
+- (IBAction)reload:(id)sender {
+}
+
+
+
+- (IBAction)changePassword:(id)sender {
+}
+
+
+
+- (IBAction)submitPasswordSheet:(id)sender {
+}
+
+
+
+- (IBAction)all:(id)sender {
+}
+
+
+
+- (IBAction)users:(id)sender {
+}
+
+
+
+- (IBAction)groups:(id)sender {
+}
+
+
+
+- (IBAction)groupFilter:(id)sender {
+}
+
+
+
+- (IBAction)search:(id)sender {
+}
+
+
+
+- (IBAction)type:(id)sender {
+}
+
+
+
+- (IBAction)group:(id)sender {
+}
+
+
+
+- (IBAction)show:(id)sender {
+}
+
+
+
+- (IBAction)clearSetting:(id)sender {
+}
+
+
+
+- (IBAction)save:(id)sender {
+}
+
+
+
+#pragma mark -
+
+- (NSInteger)numberOfRowsInTableView:(NSTableView *)tableView {
+//	return [_shownAccounts count];
+	
+	return 0;
+}
+
+
+
+- (id)tableView:(NSTableView *)tableView objectValueForTableColumn:(NSTableColumn *)tableColumn row:(NSInteger)row {
+/*	WCAccount		*account;
+
+	account = [self _accountAtIndex:row];
+
+	return [account name];*/
+	
+	return NULL;
+}
+
+
+
+- (void)tableView:(NSTableView *)tableView willDisplayCell:(id)cell forTableColumn:(NSTableColumn *)tableColumn row:(NSInteger)row {
+/*	if([[self _accountAtIndex:row] isKindOfClass:[WCUserAccount class]])
+		[cell setImage:_userImage];
+	else
+		[cell setImage:_groupImage];*/
+}
+
+
+- (NSString *)tableView:(NSTableView *)tableView stringValueForRow:(NSInteger)row {
+/*	return [[self _accountAtIndex:row] name];*/
+	
+	return NULL;
+}
+
+
+
+- (BOOL)tableView:(NSTableView *)tableView shouldSelectRow:(NSInteger)row {
+/*	if(row != [_accountsTableView selectedRow])
+		return [self _verifyUnsavedAndSelectRow:row];*/
+	
+	return YES;
+}
+
+
+
+- (void)tableViewSelectionDidChange:(NSNotification *)notification {
+/*	if([self _verifyUnsavedAndSelectRow:-1]) {
+		if([[[self connection] account] accountReadAccounts]) {
+			if([_accountsTableView numberOfSelectedRows] == 1) {
+				[self _readAccount:[self _selectedAccount]];
+			} else {
+				[_account release];
+				_account = NULL;
+				[_underlyingAccount release];
+				_underlyingAccount = NULL;
+				_editingAccount = NO;
+				_creatingAccount = NO;
+
+				[self _validateAccount:NULL];
+				[self _readFromAccount:NULL];
+			}
+		}
+		
+		[self validate];
+	}*/
+}
+
+
+
+#pragma mark -
+
+- (NSInteger)outlineView:(NSOutlineView *)outlineView numberOfChildrenOfItem:(id)item {
+/*	if(!_account)
+		return 0;
+	
+	if(!item)
+		return [_shownSettings count];
+	
+	return [[item objectForKey:WCAccountsFieldSettings] count];*/
+	
+	return 0;
+}
+
+
+
+- (id)outlineView:(NSOutlineView *)outlineView child:(NSInteger)index ofItem:(id)item {
+/*	if(!item)
+		return [_shownSettings objectAtIndex:index];
+	
+	return [[item objectForKey:WCAccountsFieldSettings] objectAtIndex:index];*/
+
+	return NULL;
+}
+
+
+
+- (id)outlineView:(NSOutlineView *)outlineView objectValueForTableColumn:(NSTableColumn *)tableColumn byItem:(id)item {
+/*	id			value;
+	
+	if(tableColumn == _settingTableColumn) {
+		return [item objectForKey:WCAccountFieldLocalizedName];
+	}
+	else if(tableColumn == _valueTableColumn) {
+		value = [_account valueForKey:[item objectForKey:WCAccountFieldName]];
+		
+		if(!value)
+			value = [_underlyingAccount valueForKey:[item objectForKey:WCAccountFieldName]];
+		
+		if([[item objectForKey:WCAccountFieldType] intValue] == WCAccountFieldNumber && [value integerValue] == 0)
+			return NULL;
+		
+		if([[item objectForKey:WCAccountFieldName] isEqualToString:@"wired.account.transfer.download_speed_limit"] ||
+		   [[item objectForKey:WCAccountFieldName] isEqualToString:@"wired.account.transfer.upload_speed_limit"])
+			value = [NSNumber numberWithInteger:[value doubleValue] / 1024.0];
+		
+		return value;
+	}*/
+
+	return NULL;
+}
+
+
+
+- (void)outlineView:(NSOutlineView *)outlineView setObjectValue:(id)object forTableColumn:(NSTableColumn *)tableColumn byItem:(id)item {
+/*	NSString	*name;
+	id			value;
+	
+	name	= [item objectForKey:WCAccountFieldName];
+	value	= object;
+	
+	if([[item objectForKey:WCAccountFieldType] intValue] == WCAccountFieldNumber)
+		value = [NSNumber numberWithInteger:[object integerValue]];
+	
+	if([name isEqualToString:@"wired.account.transfer.download_speed_limit"] ||
+	   [name isEqualToString:@"wired.account.transfer.upload_speed_limit"])
+		value = [NSNumber numberWithInteger:[value integerValue] * 1024.0];
+
+	[_account setValue:value forKey:name];
+	
+	[self touch:self];*/
+}
+
+
+
+- (void)outlineView:(NSOutlineView *)outlineView willDisplayCell:(id)cell forTableColumn:(NSTableColumn *)tableColumn item:(id)item {
+/*	if([_account valueForKey:[item objectForKey:WCAccountFieldName]])
+		[cell setFont:[[cell font] fontByAddingTrait:NSBoldFontMask]];
+	else
+		[cell setFont:[[cell font] fontByAddingTrait:NSUnboldFontMask]];
+	
+	if(tableColumn == _valueTableColumn)
+		[cell setEnabled:[self _isEditableAccount:_account]];*/
+}
+
+
+
+- (BOOL)outlineView:(NSOutlineView *)outlineView isItemExpandable:(id)item {
+//	return ([[item objectForKey:WCAccountsFieldSettings] count] > 0);
+
+	return NO;
+}
+
 @end
+
+
+
+/*@implementation WCAccountsTableColumn
+
+- (id)dataCellForRow:(NSInteger)row {
+	id		cell;
+	
+	cell = [[(NSOutlineView *) [self tableView] itemAtRow:row] objectForKey:WCAccountsFieldCell];
+	
+	if(cell)
+		return cell;
+	
+	return [super dataCellForRow:row];
+}
+
+@end*/
